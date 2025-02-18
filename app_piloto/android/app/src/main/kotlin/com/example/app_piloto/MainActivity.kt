4ed42b9c66694a116f8e3d36ca3e1b20ef71c5e5
ThiegoFerreira <thiegocpf@gmail.com>
2025-02-18 17:38:38 -0400

    Flutter

---
+++ app_piloto/android/app/src/main/kotlin/com/example/app_piloto/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.app_piloto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ app_piloto/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Menu());
+  }
+}
+
+class Menu extends StatefulWidget {
+  const Menu({super.key});
+
+  @override
+  State<Menu> createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.orangeAccent),
+      body: Container(
+        height: 150,
+        width: 150,
+        color: Colors.blueAccent,
+        child: Column(
+          children: [
+            Text(
+              "Thiego", 
+              style: TextStyle(
+                fontSize: 30, 
+                color: Colors.orangeAccent,
+                fontWeight: FontWeight.w500,
+              ),
+            ), 
+            Text(
+              "23",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.orangeAccent,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
